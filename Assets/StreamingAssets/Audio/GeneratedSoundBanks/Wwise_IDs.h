733958e60bcbf02c5648e10553fb280985107967
Kedaar Kumar <iambehindu@gmail.com>
2018-05-03 02:37:23 -0400

    Wwise - Unity Integration Test.1

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_EVENT_01 = 146835196U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMB_SOUNDBANK = 1692426037U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MONSTERS = 3858746980U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VOX = 543694036U;
+        static const AkUniqueID WEAPONS = 1467963052U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ Assets/ambient_start.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ambient_start : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AkSoundEngine.PostEvent ("amb_room_tone_01", this.gameObject);
+	}
+	
+}
