642dfde0ad9b07922b270f1beea2f7bd113b2e25
Kedaar Kumar <iambehindu@gmail.com>
2018-05-10 00:25:04 -0400

    audio asset 2D to 3D fix; event and soundbank fix

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -15,22 +15,67 @@ namespace AK
     {
         static const AkUniqueID AMB_HALLWAY = 2160321640U;
         static const AkUniqueID AMB_ROOMS = 836873062U;
+        static const AkUniqueID DOOR = 1877847629U;
+        static const AkUniqueID EXHAUST_VENT = 3855638709U;
+        static const AkUniqueID REAKTOR = 3931031181U;
         static const AkUniqueID ROTATING_PILLAR_AMB_01 = 1363715233U;
         static const AkUniqueID ROTATING_PILLAR_AMB_02 = 1363715234U;
         static const AkUniqueID ROTATING_PILLAR_AMB_03 = 1363715235U;
+        static const AkUniqueID STEAM = 1433397863U;
         static const AkUniqueID TURBINE_FAN = 3513896636U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace LIFE
+        {
+            static const AkUniqueID GROUP = 2137943U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+            } // namespace STATE
+        } // namespace LIFE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace ROOMS
+        {
+            static const AkUniqueID GROUP = 1359360203U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ROOM_01 = 3399879614U;
+                static const AkUniqueID ROOM_02 = 3399879613U;
+                static const AkUniqueID ROOM_03 = 3399879612U;
+                static const AkUniqueID ROOM_04 = 3399879611U;
+                static const AkUniqueID ROOM_05 = 3399879610U;
+                static const AkUniqueID ROOM_HALLWAY = 221854403U;
+            } // namespace SWITCH
+        } // namespace ROOMS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID GUNS = 688956056U;
         static const AkUniqueID SFX = 393239870U;
     } // namespace BANKS
 
     namespace BUSSES
     {
         static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID COMBAT = 2764240573U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MONSTERS = 3858746980U;
         static const AkUniqueID MUSIC = 3991942870U;
