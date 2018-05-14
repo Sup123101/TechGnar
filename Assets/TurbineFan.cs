1ab8c83318e0c7e52ac4fc80df796e364096b4e1
Kedaar Kumar <iambehindu@gmail.com>
2018-05-14 14:56:18 -0400

    Final Changes Implemented

---
+++ Assets/TurbineFan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurbineFan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AkSoundEngine.PostEvent("turbine_fan", this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
